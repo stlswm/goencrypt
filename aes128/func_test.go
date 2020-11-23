8954b1798351df9b8222deb9dd0bac68d06ca3cd
Wangmin <691207572@qq.com>
2020-11-23 09:43:33 +0800

    up

--- aes128/func_test.go
+++ aes128/func_test.go
@@ -1,16 +1,15 @@
 package aes128
 
 import (
-	"github.com/stlswm/goencrypt/aes128"
 	"strconv"
 	"testing"
 )
 
 func TestEncrypt(t *testing.T) {
 	for i := 1; i < 100000; i++ {
-		encStr := aes128.Encrypt(strconv.Itoa(i), "e102397d02cddc19", "b7e4eba12397a82b")
+		encStr := Encrypt(strconv.Itoa(i), "e102397d02cddc19", "b7e4eba12397a82b")
 		t.Log(encStr)
-		decStr := aes128.Decrypt(encStr, "e102397d02cddc19", "b7e4eba12397a82b")
+		decStr := Decrypt(encStr, "e102397d02cddc19", "b7e4eba12397a82b")
 		t.Log(decStr)
 		n, _ := strconv.Atoi(decStr)
 		if i != n {
@@ -24,6 +23,6 @@ func TestEncrypt(t *testing.T) {
 }
 
 func TestDecrypt(t *testing.T) {
-	decStr := aes128.Decrypt("y93amfPgKhoBR6F90l+d6Q", "e102397d02cddc19", "b7e4eba12397a82b")
+	decStr := Decrypt("y93amfPgKhoBR6F90l+d6Q", "e102397d02cddc19", "b7e4eba12397a82b")
 	t.Log(decStr)
 }
